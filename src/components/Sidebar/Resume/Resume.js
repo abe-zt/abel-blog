ddacefc00a9c34686f49b9f44603d65f5f45f3ff
Abel Zatarain <abezat@gmail.com>
2019-09-30 16:58:12 -0700

    design: swapped resume download and view link

--- src/components/Sidebar/Resume/Resume.js
+++ src/components/Sidebar/Resume/Resume.js
@@ -5,11 +5,11 @@ import downloadFile from '../../../../static/Zatarain_Resume.pdf';
 
 const Resume = () => (
   <div className={styles['resume']}> My Resume:
+    <a href={downloadFile} className={styles['view-link']}>
+       View
+    </a>
     <a href={downloadFile} download className={styles['download-link']}>
        Download
-    </a>
-      <a href={downloadFile} className={styles['view-link']}>
-       View
     </a>
   </div>
 );
