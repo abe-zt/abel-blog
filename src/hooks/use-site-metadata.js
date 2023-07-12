62e98937299276d5d5272041353a95b173cd0311
Abel Zatarain <abezat@gmail.com>
2023-07-11 21:31:31 -0700

    remove bitbucket, twitter hooks

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -14,8 +14,6 @@ const useSiteMetadata = () => {
               contacts {
                 linkedin
                 github
-                bitbucket
-                twitter
                 instagram
                 email
               }
